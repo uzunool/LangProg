90d44f1fceb9871e02da6dd4939bca3a297dea20
uzunool <uzunoolnastya@gmail.com>
2024-10-28 16:35:58 +0700

    Объекты, первая версия

---
+++ pr003.java
@@ -0,0 +1,28 @@
+class Cars {
+	int pass;    //Кол-во пассажироав
+	int volume;   //Емкость топливного бака
+	double fuel;  //Расход топлива в литрах на 100 км
+}
+
+class pr003 {
+	public static void main(String[] args) {
+		Cars Honda = new Cars();
+		Honda.pass = 2;
+	        Honda.volume = 55;
+		Honda.fuel = 20.0;
+		Cars BMW = new Cars();
+		BMW.pass = 5;
+		BMW.volume = 40;
+		BMW.fuel = 15.5;
+
+		System.out.println("Параметры Хонды: ");
+		System.out.print("Количество пассажиров: " + Honda.pass + "Объем бака: " + Honda.volume);
+		System.out.println(" Расход топлива: " + Honda.fuel);
+
+		double range;
+		range = Honda.volume/Honda.fuel*100;
+		System.out.println("Хонда на полном баке проедет " + range + "км.");
+		range = BMW.volume/BMW.fuel*100;
+		System.out.println("BMW на полном баке проедет " + range + "км.");
+	}
+}
