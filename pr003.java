b9b37d862d93a5dc285c80b05d83192c2dcc4f1b
uzunool <uzunoolnastya@gmail.com>
2024-10-28 17:02:03 +0700

    Объекты, третья версия - с параметрическим конструктором класса Cars

--- pr003.java
+++ pr003.java
@@ -2,6 +2,13 @@ class Cars {
 	int pass;    //Кол-во пассажироав
 	int volume;   //Емкость топливного бака
 	double fuel;  //Расход топлива в литрах на 100 км
+		      //Конструктор класса Cars
+	Cars(int p, int v, double f) {
+		pass = p;
+		volume = v;
+		fuel = f;
+
+	}
 	void range() {
 		System.out.println("Расстояние на полном баке: " + (volume/fuel*100));
 	}
@@ -15,14 +22,14 @@ class Cars {
 
 class pr003 {
 	public static void main(String[] args) {
-		Cars Honda = new Cars();
-		Honda.pass = 2;
-	        Honda.volume = 55;
-		Honda.fuel = 20.0;
-		Cars BMW = new Cars();
-		BMW.pass = 5;
-		BMW.volume = 40;
-		BMW.fuel = 15.5;
+		Cars Honda = new Cars(2, 55, 20.0);
+		//Honda.pass = 2;
+	        //Honda.volume = 55;
+		//Honda.fuel = 20.0;
+		Cars BMW = new Cars(5, 40, 15.5);
+		//BMW.pass = 5;
+		//BMW.volume = 40;
+		//BMW.fuel = 15.5;
 
 		System.out.println("Параметры Хонды: ");
 		System.out.print("Количество пассажиров: " + Honda.pass + "Объем бака: " + Honda.volume);
