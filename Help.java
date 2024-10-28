1757e4b183d6166b39d0774f8b1872b4cf8c7b13
uzunool <uzunoolnastya@gmail.com>
2024-10-28 16:06:34 +0700

    Третья версия Help - Добавили break и continue

--- Help.java
+++ Help.java
@@ -1,8 +1,9 @@
 class Help {
 	public static void main (String[] args)
-			throws java.io.IOException {
-			char choice, ignore;
-			do {
+	 throws java.io.IOException {
+		char choice, ignore;
+		for(;;) {
+		     do {
 
 				System.out.println("Справочная система по операторам");
 				System.out.println("  1. if");
@@ -10,12 +11,15 @@ class Help {
 				System.out.println("  3. for");
 				System.out.println("  4. while");
 				System.out.println("  5. do while");
-				System.out.println("Выберите номер раздела:  ");
+				System.out.println("  6. break");
+				System.out.println("  7. continue");
+				System.out.println("Выберите номер раздела или нажмите \"q\" для выхода:  ");
 				choice = (char) System.in.read();
 				do {
 					ignore = (char) System.in.read();
 				} while(ignore != '\n');
-			} while (choice < '1' | choice > '5');
+			} while (choice < '1' | choice > '7' & choice != 'q');
+		     	if(choice == 'q') break;
 
 			System.out.println("\n");
 			switch (choice) {
@@ -48,10 +52,18 @@ class Help {
                                         System.out.println("  Операторы");
                                         System.out.println("} while (условие);");
                                         break;
+                                case '6':
+                                        System.out.println("Оператор \"break\":\n");
+                                        System.out.println("break; или break метка;"); 
+                                        break;
+                                case '7':
+                                        System.out.println("Оператор цикла  \"continue\": \n");
+                                        System.out.println("continue; или continue метка;");
+                                        break;					
+			
 			}
-
-
-
+			System.out.println();
+		}
 
 	}
 
