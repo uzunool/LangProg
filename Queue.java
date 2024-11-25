141a382e5a3c89d4f2bd31d21c998467eb5f55c9
uzunool <uzunoolnastya@gmail.com>
2024-11-25 15:32:11 +0700

    Добавили прегружаемый констурктор для создания очереди на базе другой очереди и массива

--- Queue.java
+++ Queue.java
@@ -1,9 +1,27 @@
 class Queue {
 	private char[] q;
 	private int putloc, getloc; //указатели на индексы для помещения и извлечения символов
+	//Создается пустая очередь заданного размера
 	Queue(int size) {
 		q = new char[size]; //выделяем память под очередь нужного размера
 		putloc = getloc = 0;
+	}
+	//Создается очередь на основе другого объекта очереди
+	Queue(Queue ob) {
+		putloc = ob.putloc;
+		getloc = ob.getloc;
+		q = new char[ob.q.length];
+		//Копирование эелементов массива
+		for(int i = getloc; i<putloc; i++)
+			q[i] = ob.q[i];
+	}
+	//Создание очереди с начальными значениями на основе массива 
+	Queue(char[] a) {
+		putloc = 0;
+		getloc = 0;
+		q = new char[a.length];
+		for(int i = 0; i<a.length; i++)
+			put(a[i]);
 	}
 	//Метод для помещения символа в очередь
 	void put(char ch) {
@@ -59,5 +77,36 @@ class QDemo {
 		//Попытка доступа к закрытым переменным класса Queue
 		//bigQ.q[3] = 343;
 		//bigQ.putloc = 3;
+
+		Queue q1 = new Queue(10);
+		char[] chars = {'A', 'B', 'C'};
+		//Создание очереди на базе массива
+		Queue q2 = new Queue(chars);
+		for(i = 0; i<10; i++)
+			q1.put((char) ('D' + i));
+		//создание очереди на базе другой очереди
+		Queue q3 = new Queue(q1);
+		System.out.println("Очередь q1: ");
+		for(i=0; i<10; i++) {
+			ch = q1.get();
+			System.out.print(ch + " ");
+		}
+		System.out.println("\n");
+
+		System.out.println("Очередь q2: ");
+                for(i=0; i<3; i++) {
+                        ch = q2.get();
+                        System.out.print(ch + " ");
+                }
+		System.out.println("\n");
+
+                System.out.println("Очередь q3: ");
+                for(i=0; i<10; i++) {
+                        ch = q3.get();
+                        System.out.print(ch + " ");
+                }
+		System.out.println("\n");
+
+
 	}
 }
